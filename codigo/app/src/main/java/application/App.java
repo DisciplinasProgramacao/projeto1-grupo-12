f25a8eefb3b7e595df6c6e7003538ce1bc2e70a6
GiovanniBSDuarte <giovannibsduarte@gmail.com>
2023-03-13 08:53:57 -0300

    Menu para auxilio na tarefa

--- codigo/app/src/main/java/application/App.java
+++ codigo/app/src/main/java/application/App.java
@@ -1,47 +1,46 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-/**
- * Hello world!
- */
 public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+    
+    static Scanner entrada = new Scanner(System.in);
+   
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("banana", 10, 25, 20, 2);
-        Produto p2 = new Produto("macarrao", 20, 30, 30, 2);
-        Produto p3 = new Produto("linguiça", 20, 30, 3, 2);
-        Produto p4 = new Produto("carne", 20, 30, 3, 2);
-
-        Estoque e = new Estoque();
-
-        List<Produto> produtosAbaixoDoEstoque = new ArrayList<>();
-
-        e.reporEstoque(p1);
-        e.reporEstoque(p2);
-        e.reporEstoque(p3);
-        e.reporEstoque(p4);
-
-        produtosAbaixoDoEstoque = e.QuaisMenorQueMinimo();
-
-        System.out.println(e.getQuantidade());
-        System.out.println(e.calcularValorEstoque());
-        e.retirarEstoque(p3);
         
+    }
 
-        for(int i=0; i< produtosAbaixoDoEstoque.size(); i++) {
-            System.out.println(produtosAbaixoDoEstoque.get(i).getDescricao());
-        }
+    public void Menu(){
+        int x;
+        do{
+            System.out.println("Escolha o que quer fazer:");
+            System.out.println("1 --> Venda e compra de produtos;");
+            System.out.println("2 --> Consultar dados de produtos;");
+            System.out.println("3 --> Calcular balanço da empresa;");
+            System.out.println("0 --> Encerrar programa.");
+    
+            x = entrada.nextInt();
+            switch (x){
+                case 0:
+                break;
+    
+                case 1:
+    
+                break;
+    
+                case 2:
+    
+                break;
+    
+                case 3:
+    
+                break;
 
-        e.imprimirEstoque();
-
+                default:
+                System.out.println("Número inválido.");
+            }
+        }while(x != 0);
+        
     }
 }
